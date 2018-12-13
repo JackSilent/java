bf272d7304cf7ac8d152211963e85e0c393c5f6d
jack_glb <jack_glb@qq.com>
2018-12-13 16:56:50 +0800

    feign-service

---
+++ service-feign/src/main/java/com/jack/FeignApplication.java
@@ -0,0 +1,16 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author jack
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class FeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class,args);
+    }
+}
